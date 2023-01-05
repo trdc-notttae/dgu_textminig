512b2ef245136e00df1870df041982c09b6cfda5
arongi2436 <arongi2436@gmail.com>
2023-01-04 20:51:54 -0500

    no_rquirements
    no_rquirements

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
 name="dgu_dtm", # 이 패키지를 설치/삭제할 때 사용할 이름을 의미한다. 이 이름과 import할 때 쓰이는 이름은 다르다.
-version="0.0.1",
-
+version="0.0.2",
 url="https://github.com/dhkwak/simple_scheduler",
-install_requires=requirements
 )
